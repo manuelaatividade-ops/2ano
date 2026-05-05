f9705d00eefd913f649791a9df39e81ea1713760
manuelaatividade-ops <manuelaatividade@gmail.com>
2026-05-05 08:28:49 -0300

    Create Produto.cs

---
+++ Projeto/Produto.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Manuelaalfa
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        public double ValorTotalEmEstoque ()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionaProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+                 + ", $"
+                 + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                 + ", "
+                 + Quantidade
+                 + "unidades, Total: $"
+                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
