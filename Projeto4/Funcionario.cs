5e5b8b199de98de3df736781c78fd4d5a29b0fb2
manuelaatividade-ops <manuelaatividade@gmail.com>
2026-05-07 10:46:59 -0300

    Create Funcionario.cs

---
+++ Projeto4/Funcionario.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Manuelaalfapt4
+{
+    internal class funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentoSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100);
+        }
+        public override string ToString()
+        {
+            return Nome
+                + ", R$ "
+                + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
